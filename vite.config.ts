dfddac7f0ecf6c83b0d8ffccac25945f91841aee
ruvvet <finej01@gmail.com>
2025-03-23 00:54:40 -0400

    google cloud host instead

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { vitePlugin as remix } from '@remix-run/dev';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-declare module "@remix-run/node" {
+declare module '@remix-run/node' {
   interface Future {
     v3_singleFetch: true;
   }
@@ -11,14 +11,15 @@ declare module "@remix-run/node" {
 export default defineConfig({
   plugins: [
     remix({
+      // ssr: false,
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
         v3_singleFetch: true,
         v3_lazyRouteDiscovery: true,
-      },
+      }
     }),
-    tsconfigPaths(),
-  ],
+    tsconfigPaths()
+  ]
 });
